a013be04cb6c64cd1ca9a7a2d83fe47d7e45c392
LEE <459322602@qq.com>
2016-08-23 14:51:12 +0800

    '一些更新'

--- src/user/pages/aboutUs.vue
+++ src/user/pages/aboutUs.vue
@@ -1,40 +1,35 @@
 <template>
-  <div class='aboutUs'>
-      <x-header :left-options="{showBack: true}">关于我们 </x-header>
-    
-  </div>
-  
+	<div class='aboutUs'>
+			<x-header :left-options="{showBack: true}">关于我们 </x-header>
+	</div>
 </template>
 
 <script>
 import {XHeader,XInput,Group} from 'vux'
 
-
 export default {
-  components: {
-    XHeader,XInput,Group
-  },
-  methods: {
-    _complete(){
-      alert(1);
-    }
-    
-    
-  }
+	components: {
+		XHeader,XInput,Group
+	},
+	methods: {
+		_complete(){
+			alert(1);
+		}
+	}
 }
 </script>
 
 <style lang="less">
 .aboutUs{
-  .vux-header{
-    color:#fff;
-    background-color:#4bb7aa;
-  }
-  .weui_cells{margin-top:0;}
-  .vux-no-group-title{margin-top:0;}
-  .vux-header .vux-header-right a{color:#fff;}
-  .vux-header .vux-header-left, .vux-header .vux-header-right{font-size:16px;}
-  .vux-header .vux-header-left .vux-header-back:before{border-color:#fff;}
-  .vux-header .vux-header-title, .vux-header h1{margin-left:88px;}
+	.vux-header{
+		color:#fff;
+		background-color:#4bb7aa;
+	}
+	.weui_cells{margin-top:0;}
+	.vux-no-group-title{margin-top:0;}
+	.vux-header .vux-header-right a{color:#fff;}
+	.vux-header .vux-header-left, .vux-header .vux-header-right{font-size:16px;}
+	.vux-header .vux-header-left .vux-header-back:before{border-color:#fff;}
+	.vux-header .vux-header-title, .vux-header h1{margin-left:88px;}
 }
 </style>
