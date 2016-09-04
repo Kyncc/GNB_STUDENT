a2dde27b7dfc1ea583e3fb3b3e9ff60848a51d36
LEE <459322602@qq.com>
2016-09-04 22:07:49 +0800

    '封装Loading和Toast完毕'

---
+++ src/config/whole.js
@@ -0,0 +1,26 @@
+import store from '../store'
+
+/** 
+*   页面正在请求
+*/
+export function busy(){
+    store.dispatch('GET_LOADING', true);
+}
+
+/** 
+*   页面请求结束
+*/
+export function leave(){
+    store.dispatch('GET_LOADING', false);
+}
+
+/** 
+*   Toast公共方法
+*/
+export function toast(str){
+     store.dispatch('GET_TOAST_SHOW', true); 
+     store.dispatch('GET_TOAST_MESSAGE',str);
+     setTimeout(()=>{
+          store.dispatch('GET_TOAST_SHOW', false); 
+     },1500); 
+}
