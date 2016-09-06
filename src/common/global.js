c54593da0f4042d701b41fc9aa885d28806d3309
LEE <459322602@qq.com>
2016-09-06 11:46:27 +0800

    'password页面增加验证'

---
+++ src/common/global.js
@@ -0,0 +1,28 @@
+import {
+  GET_TOAST_MESSAGE,
+  GET_TOAST_SHOW,
+  GET_LOADING
+} from './mutationTypes'
+
+const state = {
+    toastMsg:'',
+    toastShow:false,
+    isLoading:false
+}
+
+const mutations = {
+    GET_TOAST_MESSAGE (state, msg) {
+      state.toastMsg = msg
+    },
+    GET_TOAST_SHOW (state, status) {
+      state.toastShow = status
+    },
+    GET_LOADING (state, status) {
+      state.isLoading = status
+    }
+}
+
+export default {
+  state,
+  mutations
+}
