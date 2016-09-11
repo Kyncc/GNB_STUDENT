18abd6f7ade19565ef60906bb108a91f13709c61
LEE <459322602@qq.com>
2016-09-11 09:52:16 +0800

    '修改滚动'

--- src/collect/pages/detail.vue
+++ src/collect/pages/detail.vue
@@ -1,78 +1,100 @@
 <template>
-	<div class='collectDetail  vux-scroller-header-box'>
-		<div style="height:46px;">
-			<x-header :left-options="{showBack: true}" style="position:fixed;left:0;top:0;width:100%;" class="vux-scroller-header">我的收藏<a slot="right" v-touch:tap="_remove">移除本题</a></x-header>
-		</div>
-		
-        <div class="weui_panel weui_panel_access exerciseDetail">
-			<div class="weui_panel_hd">
-				  <flexbox :gutter="0" wrap="wrap">
-					<flexbox-item :span="3/5" style="color:#4bb7aa">收藏题1</flexbox-item>
-				</flexbox>				
-			</div> 
-			<div class="weui_panel_bd"> 
-				<div class="weui_media_bd weui_media_box "> 
-					<p class="weui_media_desc">
-						小敏家、学校、邮局、图书馆坐落在一条东西走向的大街上，依次记为A，B，C，D，学校位于小敏家西150米，邮局位于小敏家东100米，图书馆位于小敏家西400米．<br/> 
-						（1）用数轴表示A，B，C，D的位置；<br/>
-						（2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
-					</p>
-				</div> 
-				<div class="weui_media_bd weui_media_box options">
-					<p class="weui_media_desc"> 
-							A.(-4.2)
-					</p>
-					<p class="weui_media_desc"> 
-							B.(-4.2)
-					</p>
-					<p class="weui_media_desc"> 
-							C.(-4.2)
-					</p>
-					<p class="weui_media_desc"> 
-							D.(-4.2)
-					</p>
-				</div>  
-			</div>
+	<view-box v-ref:view-box class="collectDetail">
+
+		<div slot="header" style="position:absolute;left:0;top:0;width:100%;z-index:100" >
+			<x-header :left-options="{showBack: true}">我的收藏<a slot="right" v-touch:tap="_remove">移除本题</a></x-header>
 		</div>
 
-		<div class="weui_panel weui_panel_access exerciseDetail">
-			<div class="weui_panel_hd">
-				  <flexbox :gutter="0" wrap="wrap">
-					<flexbox-item :span="3/5" style="color:#4bb7aa">本题解析</flexbox-item>
-					<flexbox-item :span="1/5">
-					</flexbox-item>
-					<flexbox-item :span="1/5">
-						<a href="javascript:;" style="width:100%" v-touch:tap="_correct">纠错</a>
-					</flexbox-item>
-				</flexbox>				
-			</div> 
-			<div class="weui_panel_bd"> 
-				<div class="weui_media_bd weui_media_box "> 
-					<p class="weui_media_desc">
-						小敏家、学校、邮局、图书馆坐落在一条东西走向的大街上，依次记为A，B，C，D，学校位于小敏家西150米，邮局位于小敏家东100米，图书馆位于小敏家西400米．<br/> 
-						（1）用数轴表示A，B，C，D的位置；<br/>
-						（2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
-					</p>
+		<div style="padding-top:46px;">
+			<!--内容-->
+			<div class="weui_panel weui_panel_access exerciseDetail">
+				<div class="weui_panel_hd">
+					<flexbox :gutter="0" wrap="wrap">
+						<flexbox-item :span="3/5" style="color:#4bb7aa">收藏题1</flexbox-item>
+					</flexbox>				
+				</div>
+				<!--题目整体--> 
+				<div class="weui_panel_bd"> 
+					<!--题目-->
+					<div class="weui_media_bd weui_media_box "> 
+						<p class="weui_media_desc">
+							小敏家、学校、邮局、图书馆坐落在一条东西走向的大街上，依次记为A，B，C，D，学校位于小敏家西150米，邮局位于小敏家东100米，图书馆位于小敏家西400米．<br/> 
+							（1）用数轴表示A，B，C，D的位置；<br/>
+							（2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
+						</p>
+					</div> 
+					<!--选项-->
+					<div class="weui_media_bd weui_media_box options">
+						<p class="weui_media_desc"> 
+								A.(-4.2)
+						</p>
+						<p class="weui_media_desc"> 
+								B.(-4.2)
+						</p>
+						<p class="weui_media_desc"> 
+								C.(-4.2)
+						</p>
+						<p class="weui_media_desc"> 
+								D.(-4.2)
+						</p>
+					</div>  
+				</div>
+			</div>
+			<!--解析--> 
+			<div class="weui_panel weui_panel_access exerciseDetail">
+				<div class="weui_panel_hd">
+					<flexbox :gutter="0" wrap="wrap">
+						<flexbox-item :span="2/5" style="color:#4bb7aa">本题解析</flexbox-item>
+						<flexbox-item :span="1/5"></flexbox-item>
+						<flexbox-item :span="2/5" style="text-align:right">
+						 	<x-button mini type="primary" plain @click="_correct">点我纠错</x-button>
+						</flexbox-item>
+					</flexbox>				
 				</div> 
+				<!--解析主体--> 
+				<div class="weui_panel_bd"> 
+					<div class="weui_media_bd weui_media_box "> 
+						<p class="weui_media_desc">
+							小敏家、学校、邮局、图书馆坐落在一条东西走向的大街上，依次记为A，B，C，D，学校位于小敏家西150米，邮局位于小敏家东100米，图书馆位于小敏家西400米．<br/> 
+							（1）用数轴表示A，B，C，D的位置；<br/>
+							（2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
+							（2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
+							（2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
+							（2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
+							（2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
+							（2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
+						</p>
+					</div> 
+				</div>
 			</div>
 		</div>
+	</view-box>
+	<confirm :show.sync="show" confirm-text="是" cancel-text="否" title="确定将此题移除收藏么?" @on-confirm="onAction('是')" @on-cancel="onAction('否')"></confirm>
 </template>
 
 <script>
-import {XHeader, Panel,Flexbox,FlexboxItem,XButton} from 'vux'
+import {XHeader,Flexbox,FlexboxItem,XButton,Confirm,ViewBox} from 'vux'
 // import './collect.less'
 
 export default {
 	components: {
-		XHeader, Panel,Flexbox,FlexboxItem,XButton
+		XHeader,Flexbox,FlexboxItem,XButton,Confirm,ViewBox
 	},
 	methods: {
 		_remove(){
-			alert(1);
+			 this.show = true
 		},
 		_correct(){
 			this.$router.go('/collect/correct')
+		},
+		onAction:(type)=>{
+			alert(type);
 		}
+	},
+	data(){
+		return{
+			 show: false
+		} 
 	}
 }
 </script>
--- src/collect/pages/index.vue
+++ src/collect/pages/index.vue
@@ -27,6 +27,30 @@
                 </div>  
             </div>
             
+            <div class="weui_panel weui_panel_access exerciseExampleList">
+                 <div class="weui_panel_hd"><x-button type='primary' mini>收藏题1</x-button>2.5 函数零点判定原理</div> 
+                 <div class="weui_panel_bd"> 
+                    <a class="weui_media_box weui_media_appmsg" href="#!/collect/detail">  
+                        <div class="weui_media_bd"> 
+                            <p class="weui_media_desc">
+                                小敏家、学校、邮局、图书馆坐落在一条东西走向的大街上，依次记为A，B，C，D，学校位于小敏家西150米，邮局位于小敏家东100米，图书馆位于小敏家西400米．<br/> 
+                                （1）用数轴表示A，B，C，D的位置；<br/>
+                                （2）一天小敏从家里先去邮局寄信后，再以每分钟50米的速度往图书馆方向走了约8分钟．试问这时小敏约在什么位置？距图书馆和学校各约多少米？<br/>
+                            </p> 
+                        </div> 
+                    </a>  
+                    <div class="weui_panel_ft" >
+                         <flexbox :gutter="0" wrap="wrap">
+                             <flexbox-item :span="1/2">收藏时间：2016-01-14</flexbox-item>
+                             <flexbox-item :span="1/4"></flexbox-item>
+                             <flexbox-item :span="1/4" style="text-align:right"> 难度：3</flexbox-item>
+                         </flexbox>
+                    </div>
+                </div>  
+            </div>
+
+
+            
             <div class="weui_panel weui_panel_access exerciseExampleList ">
                  <div class="weui_panel_hd"><x-button type='primary' mini>收藏题2</x-button>2.6 函数零点判定原理</div> 
                  <div class="weui_panel_bd"> 
