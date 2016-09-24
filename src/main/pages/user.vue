ce520faaaed5a1953709e56098ad2c320cec43e4
wcl <434777723@qq.com>
2016-09-24 22:59:16 +0800

    上传头像 没回调

--- src/main/pages/user.vue
+++ src/main/pages/user.vue
@@ -39,10 +39,10 @@
   </scroller>
   <actionsheet :show.sync="showsheet" cancel-text="取消" :menus="menus" @on-click-menu="_uploadclick" show-cancel></actionsheet>
   <confirm :show.sync="show" confirm-text="确定" cancel-text="取消" title="确定退出当前登陆账号么" @on-confirm="onAction('确认')" @on-cancel="onAction('取消')"></confirm>
-  <form  v-show="false" enctype="multipart/form-data" action="http://122.114.55.74:8070/v1/student/user/headImg" method="post">
+  <form v-show="false" enctype="multipart/form-data" action="http://122.114.55.74:8070/v1/student/user/headImg" method="post">
     <input type="file" id="choosefile" />
     <input type="submit" value="上传文件" id="submitBtn" />
-</form>
+  </form>
 </view-box>
 </template>
 
@@ -50,6 +50,7 @@
 import {XHeader,Cell,Group,Confirm,Scroller,Actionsheet,ViewBox} from 'vux'
 import {quitToken} from '../actions.js'
 import {fetchToken } from '../../user/getters.js'
+import * as _ from '../../config/whole.js'
 
 export default {
   components: {
@@ -80,8 +81,49 @@ export default {
         this.showsheet = true
     },
     _uploadclick (key) {
+        var server = "http://demo.dcloud.net.cn/helloh5/uploader/upload.php";
+		var files = [];
+        var index = 1;
          if(key == 'menu2'){
-             document.getElementById('choosefile').click();
+             //document.getElementById('choosefile').click();
+             plus.gallery.pick(function(p) {
+                var fe = document.getElementById("files");
+				var li = document.createElement("li");
+				var n = p.substr(p.lastIndexOf('/') + 1);
+				li.innerText = n;
+				fe.appendChild(li);
+				files.push({
+					name: "uploadkey" + index,
+					path: p
+				});
+				index++;
+				empty.style.display = "none";
+                var task = plus.uploader.createUpload(server, {
+						method: "POST"
+					},
+					function(t, status) { //上传完成
+                        _.toast(status)
+						if(status == 200) {
+                            _.toast('上传成功')
+							outLine("上传成功：" + t.responseText);
+							plus.storage.setItem("uploader", t.responseText);
+
+						} else {
+                            _.toast('上传失败')
+							outLine("上传失败：" + status);
+						}
+					}
+				);
+				task.addData("client", "HelloH5+");
+				task.addData("uid", Math.floor(Math.random() * 100000000 + 10000000).toString());
+				for(var i = 0; i < files.length; i++) {
+					var f = files[i];
+					task.addFile(f.path, {
+						key: f.name
+					});
+				}
+				task.start();
+			});
            }
          }
   },
