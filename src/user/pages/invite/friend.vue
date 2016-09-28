6ae40c991df5ae686204819dc4a6222408be4f60
LEE <459322602@qq.com>
2016-09-28 21:58:58 +0800

    '更新'

--- src/user/pages/invite/friend.vue
+++ src/user/pages/invite/friend.vue
@@ -9,7 +9,7 @@
 					<img slot="icon" width="30" style="display:block;margin-right:5px;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII=" :src="item.headImg">
 				</cell>
 			</group>
-			<infinite-loading :on-infinite="onInfinite" spinner="waveDots">
+			<infinite-loading :on-infinite="onInfinite" spinner="spiral">
 				<span slot="no-more" style="color:#4bb7aa;">
 					<i class="icon iconfont icon-comiiszanwushuju" style="font-size:1.5rem;margin-right:.2rem"></i>
 					<p style="font-size:1rem;display:inline-block;">没有更多数据了</p>
