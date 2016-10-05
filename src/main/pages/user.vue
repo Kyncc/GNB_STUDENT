39114178c1b892a62179d54cf6a14c233ffe57ad
LEE <459322602@qq.com>
2016-10-05 13:26:49 +0800

    '更细'

--- src/main/pages/user.vue
+++ src/main/pages/user.vue
@@ -78,7 +78,7 @@ export default {
 			cmr.captureImage(function(p) {
 				plus.io.resolveLocalFileSystemURL(p, function(entry) {
 					self.setHeadPhoto(entry.toLocalURL())
-                    self.$router.go('user/photo')
+                    self.$router.go('/main/user/photo');
 				})
 			});
         },
@@ -86,7 +86,7 @@ export default {
             let self = this
             plus.gallery.pick(function(e) {
                 self.setHeadPhoto(e.files[0])
-                self.$router.go('user/photo')
+                self.$router.go('/main/user/photo');
 			}, function(e) {
                  _.toast("取消选择图片")
 			}, {
