6d8ff5e1235efafa808a579ac5bc4c988b83f804
Hope <walykadyb@vip.qq.com>
2016-10-31 22:17:04 +0800

    '组件更新'

---
+++ src/components/changesub/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <mt-popup :visible.sync="show" popup-transition="popup-fade" class="gnb-changeSub">
+      <p >数学</p>
+      <p >物理</p>
+      <p >化学</p>
+    </mt-popup>
+</template>
+
+
+<script>
+import Vue from 'vue'
+import { Popup } from 'mint-ui'
+import './index.less'
+
+Vue.component('mt-popup', Popup);
+
+module.default = {
+    props: {
+        show: {
+            type:Boolean,
+            default: true
+        }
+    },
+    methods: {
+
+
+
+    }
+}
+</script>
+
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import GnbChangeSub from './changesub/index.vue'
+
+
+const GNB = {
+    GnbChangeSub
+}
+
+module.exports = GNB;
