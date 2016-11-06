7391bb4d69dfe5ca20d07700d7bf96139ad1eaaa
Hope <walykadyb@vip.qq.com>
2016-11-06 17:42:52 +0800

    '更新'

---
+++ src/components/accordion/index.vue
@@ -0,0 +1,29 @@
+<template >
+    <div class="container">
+        <section class="ac-container">
+            <div v-for="items in list" class="gnb-accordion">
+                <input :id="'ac-'+$index" name="accordion-1" type="checkbox" :checked="$index == '0'" />
+                <label class="header" :for="'ac-'+$index">{{items.name}}<span class="with_arrow"></span></label>
+                <article class="ac-small">
+                    <group>
+                        <cell v-for="item in items.chapter_list" :title="item.name" :link="link+item.chapter_id"></cell>
+                    </group>
+                </article>
+            </div>
+        </section>
+    </div>
+</template>
+
+
+<script>
+import {Cell, Group} from 'vux'
+import './index.less'
+
+export default {
+    name:'accordion',
+    components:{
+        Cell,Group
+    },
+    props: ['list','link']
+}
+</script>
