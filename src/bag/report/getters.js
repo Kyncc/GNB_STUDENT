ca404315163105ffd479d4f7b0de603f208881d0
Hope <hopefullstack@gmail.com>
2016-12-25 21:21:34 +0800

    '我的模块调整'

---
+++ src/bag/report/getters.js
@@ -0,0 +1,4 @@
+export const reportChapter = (state) => state.report.chapter
+export const reportDetail = (state) => state.report.detail
+export const reportScoll = (state) => state.report.scoll
+export const reportSubjectId = (state) => state.report.subjectId
