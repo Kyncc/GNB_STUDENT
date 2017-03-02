222a72e37e2c7f5ead22fcc2acec0879e26b6cc5
Hope <hopefullstack@gmail.com>
2017-03-02 21:21:02 +0800

    增加刷题提示

--- src/featrues/brush/pages/list.vue
+++ src/featrues/brush/pages/list.vue
@@ -1,8 +1,7 @@
 <template>
   <view-box class="brushList">
     <div slot="header" style="position:absolute;left:0;top:0;width:100%;z-index:100">
-      <x-header :left-options="{showBack: true}">刷题列表
-      </x-header>
+      <x-header :left-options="{showBack: true}">刷题列表</x-header>
       <header v-if="brushList" class="sectionHeader">
         <p class="ellipsis">{{brushList.chapterName}}</p>
         <font class="ellipsis"><b>{{brushList.count}}/{{brushList.total}}</b></font>
@@ -33,9 +32,9 @@
           </div>
         </a> 
         <div class="abandon">
-          <span @click="_abandon('1',item.exercises_id,$index)">斩题</span>
           <span @click="_abandon('2',item.exercises_id,$index)">弃题</span>
           <span @click="_abandon('3',item.exercises_id,$index)">刷题</span>
+          <span @click="_abandon('1',item.exercises_id,$index)">斩题</span>
         </div>
       </div>
       </template>
@@ -48,17 +47,21 @@
       </infinite-loading>
     </div>
   </view-box>
-
+  <confirm :show.sync="confirmShow" confirm-text="确定" cancel-text="取消" :title="confrimTitle" @on-confirm="_onAction">
+    <p style="padding:.35rem 0 ;font-size:16px;" @click="_noticeAgain" :class="notAgain ? 'notAgain':''">
+      <b><i class="icon iconfont exampleIcon icon-correct"></i></b>  选我以后不在提示
+    </p>
+  </confirm>
 </template>
 
 <script>
-import {XHeader,Panel,Flexbox,FlexboxItem,XButton,ViewBox,ButtonTab,ButtonTabItem} from 'vux'
+import {XHeader,Panel,Flexbox,FlexboxItem,XButton,ViewBox,ButtonTab,ButtonTabItem,Confirm} from 'vux'
 import InfiniteLoading from 'vue-infinite-loading'
 import { mapActions,mapGetters  } from 'vuex'
 
 export default {
   components: {
-    XHeader,XButton,InfiniteLoading,
+    XHeader,XButton,InfiniteLoading,Confirm,
     Panel,Flexbox,FlexboxItem,ViewBox,ButtonTab,ButtonTabItem
   },
   route: {
@@ -83,13 +86,26 @@ export default {
       this.setBrushListScroll(document.getElementsByClassName("vux-fix-safari-overflow-scrolling")[0].scrollTop);
       this.$router.go(`/example/${this.brushSubjectId}/${id}`);
     },
-    _abandon(type,id,index){
-        let parm = {
-          'id':id,
-          'status': type,
-          'index':index
+    _abandon(status,id,index){
+        this.selectItem.id = id;
+        this.selectItem.status = status;
+        this.selectItem.index = index;
+        if(localStorage.getItem("brushFirst")){
+          this.brushAction(this.selectItem)
+        }else{
+          this.confirmShow = true
         }
-        this.brushAction(parm)
+    },
+    //动作的提示
+    _noticeAgain(){
+      this.notAgain = !this.notAgain
+    },
+    //确认不提示
+    _onAction(){
+      if(this.notAgain){
+        localStorage.setItem("brushFirst","true")
+      }
+      this.brushAction(this.selectItem)
     },
     _onInfinite(){
       this.getBrushList()
@@ -104,7 +120,32 @@ export default {
     }
   },
   computed:{
-    ...mapGetters(['brushList','brushListIsReset','brushSubjectId','brushListScroll'])
+    ...mapGetters(['brushList','brushListIsReset','brushSubjectId','brushListScroll']),
+    confrimTitle(){
+      if(this.selectItem.status == '2'){
+        return '真的很简单，确定放进弃题本？'
+      }else if(this.selectItem.status == '3'){
+        return '真的需要练习，确定放进刷题本？'
+      }else{
+        return '真的很难，确定放进斩题本？'
+      }
+    }
+  },
+   data(){
+    return {
+      confirmShow:false,
+      selectItem:{
+        'id':'',
+        'status': '',
+        'index':''
+      },
+      notAgain: false,
+    }
   }
 }
 </script>
+<style scoped>
+.notAgain{
+  color:#4bb7aa;
+}
+</style>
