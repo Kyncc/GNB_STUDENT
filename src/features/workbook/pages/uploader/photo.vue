d6c88da04907125562c20b0bacd35d312aa14418
Hope <hopefullstack@gmail.com>
2017-08-14 08:28:13 +0800

    照片旋转

--- src/features/workbook/pages/uploader/photo.vue
+++ src/features/workbook/pages/uploader/photo.vue
@@ -6,7 +6,7 @@
     <div>
       <img ref="result" :src="workbookUploader.camera" style="width:100%;"/>
       <!-- <img ref="result" src="http://img.guinaben.com/workbookPic/429answer5810/1/Untitled.FR12%20-%200001.png?imageMogr2/auto-orient/thumbnail/!50p/format/jpg/interlace/1/quality/30|imageslim" style="width:100%;"/>-->
-      <div class='rotate' @click="cropper.rotate(90)"><img src="../../assets/rotate-right.png"></div>
+      <div class='rotate' @click="cropper.rotate(-90)"><img src="../../assets/rotate-right.png"></div>
     </div>
   </view-box>
 </template>
