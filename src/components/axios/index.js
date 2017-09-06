c87fadda636abe913e226ab5cf3cae229964fbf8
Hope <hopefullstack@gmail.com>
2017-09-06 17:35:45 +0800

    更新添加

--- src/components/axios/index.js
+++ src/components/axios/index.js
@@ -5,10 +5,13 @@ import qs from 'qs'
 axios.defaults.timeout = 10000
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
 axios.defaults.baseURL = 'https://www.guinaben.com/student/'
-// axios.defaults.baseURL = 'http://192.168.13.222:90/edu_api/student'
 
 // POST传参序列化
 axios.interceptors.request.use((config) => {
+  config.params = {
+    ...config.params,
+    VERSION: '3.3.0'
+  }
   if (config.method === 'post') {
     config.data = qs.stringify(config.data, {arrayFormat: 'brackets'})
   }
@@ -31,6 +34,19 @@ axios.interceptors.response.use((res) => {
       }
     }, 500)
     return Promise.reject(res)
+  } else if (res.data.code === 403) {
+    // 升级的代码
+    Vue.$vux.alert.show({
+      title: '更新',
+      content: '据说新增功能666',
+      onHide () {
+        if (/iphone|ipad|ipod/.test(navigator.userAgent.toLowerCase())) {
+          plus.runtime.openURL('itms-apps://itunes.apple.com/cn/app/gui-na-ben-xue-sheng-duan/id1184077595?l=en&mt=8')
+        } else {
+          window.location.href = 'market://details?id=com.sanbao.guinaben.student'
+        }
+      }
+    })
   } else if (res.data.code !== 200) {
     Vue.$vux.toast.show({text: res.data.msg, type: 'text', time: 1500, position: 'bottom'})
     return Promise.reject(res)
